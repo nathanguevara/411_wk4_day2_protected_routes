e93a3182a867c1a8ef237afbc7a109455fb9408d
nathanguevara <nathan.guevara@gmail.com>
2022-08-28 13:12:16 -0500

    complete

--- src/Router.js
+++ src/Router.js
@@ -7,20 +7,31 @@ import Car from './components/Car'
 import Login from './components/Login'
 
 // Write checkAuth function here
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies["loggedIn"] ? true : false
+}
 // Check the cookies for a cookie called "loggedIn"
 
 
 // Write ProtectedRoute function here
+const ProtectedRoute = (props) => {
+    const { component: Component } = props;
+    
+    return (
+       checkAuth() ? <Component /> : <Navigate to="/login" />
+)
+}
 
 
 const Router = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/car/:id" element={<Car/>} />
-        </Routes>
+    <Routes>
+      <Route path="/" element={<ProtectedRoute component={Home} />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/about" element={<ProtectedRoute component={About} />} />
+      <Route path="/car/:id" element={<ProtectedRoute component={Car} />} />
+    </Routes>
     );
 };
 
